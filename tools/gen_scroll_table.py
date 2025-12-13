a94cbc87ae2864cd820f623f46e743bba15a5b20
Jean-François Fabre <jotd666@gmail.com>
2025-12-13 23:29:22 +0100

    preparing OCS version

--- tools/gen_scroll_table.py
+++ tools/gen_scroll_table.py
@@ -1,7 +1,7 @@
 import bitplanelib
 import os,pathlib
 
-fkfkfkfkfk this aint the one to use!! use tracknfield one!
+
 this_dir = pathlib.Path(os.path.abspath(__file__)).parent
 src_dir = this_dir / os.pardir / "src" / "amiga"
 
@@ -9,10 +9,22 @@ src_dir = this_dir / os.pardir / "src" / "amiga"
 
 
 # table with fine shift + byte offset
-def doit(width):
+def doit(width,dual_playfield):
+    static_x_shift = width // 2  # shift other bitplane by half to get clipping effect on sprites
     asm_output = src_dir / f"scroll_table_{width}.68k"
     wmask = width-1
     scroll_table = [0]*512
+
+
+    if dual_playfield:
+        static_lsb = (static_x_shift-1) >> 4
+        static_msb = (static_x_shift-1) & 0xF
+
+
+        # bob playfield
+        other_plane_mask_1 = static_lsb << 2
+        other_plane_mask_2 = static_msb
+
 
     items = []
     for x in range(0,512):
@@ -21,9 +33,13 @@ def doit(width):
         # pre-encode shift for bplcon
 
         shiftval_msb = ((shift&(wmask & 0x30))>>2)      # 2 high bits H7 H6
-        items.append(shiftval_msb | (shiftval_msb<<4)) # put same shift for both "playfields"
         shiftval_lsb = (shift&0xF)                      # 4 low bits H5->H2
-        items.append((shiftval_lsb<<4) | shiftval_lsb) # put same shift for both "playfields"
+        if not dual_playfield:
+            other_plane_mask_1 = shiftval_msb
+            other_plane_mask_2 = shiftval_lsb
+
+        items.append((shiftval_msb<<4)|other_plane_mask_1)
+        items.append((shiftval_lsb<<4)|other_plane_mask_2)
 
         items.append(0)
         items.append(offset)
@@ -34,5 +50,5 @@ def doit(width):
     return items
 
 if __name__ == "__main__":
-    doit(width = 32)   # FMODE=1
-    doit(width = 64)   # FMODE=3
+    doit(width = 64, dual_playfield = True)   # FMODE=3
+    doit(width = 16, dual_playfield = False)   # FMODE=0
